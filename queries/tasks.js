bacbf4b99ec462d75c71638357a0097db67ae22c
Rubby <rubenslombard@pursuit.org>
2023-11-06 12:18:06 -0500

    added controller, queries,

---
+++ queries/tasks.js
@@ -0,0 +1,19 @@
+const db = require('../db/dbConfig')
+
+const getAllTasks = async () => {
+
+    try {
+        const allTasks = await db.any("SELECT * FROM tasks")
+        return allTasks
+    }
+    catch (error) {
+        return error
+        
+    }
+}
+
+
+
+
+
+module.exports = { getAllTasks }
