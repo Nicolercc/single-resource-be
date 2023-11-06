b384eaabcf3f75a19fd0241b94865c2a4aa4dc0a
Nicole Rodriguez <nicolerodriguezcab@gmail.com>
2023-11-06 11:51:05 -0500

    database and table creation

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS tasks_dev;
+CREATE DATABASE tasks_dev;
+
+\c tasks_dev;
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY, 
+    name TEXT NOT NULL,
+    description VARCHAR(200) DEFAULT NULL, 
+    deadline DATE DEFAULT NULL, 
+    category VARCHAR(50) DEFAULT 'Uncategorized', 
+    assigned_to VARCHAR(70) DEFAULT 'Unassigned', 
+     status TEXT DEFAULT 'Pending' 
+ );
