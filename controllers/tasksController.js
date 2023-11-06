dcf71fb0759ad2f6bee2ea3283af07f5b270c007
Nicole Rodriguez <nicolerodriguezcab@gmail.com>
2023-11-06 13:01:09 -0500

    added show and create routes and queries

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const tasks = express.Router();
-const { getAllTasks } = require("../queries/tasks");
+const { getAllTasks, getTask, createTask } = require("../queries/tasks");
 
-//GET -
+//GET - Index
 tasks.get("/", async (req, res) => {
   const allTasks = await getAllTasks();
   if (allTasks) {
@@ -10,6 +10,25 @@ tasks.get("/", async (req, res) => {
   } else {
     res.status(404).json({ error: "Tasks Array is empty" });
   }
+});
+
+//GET - show
+tasks.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const oneTask = await getTask(id);
+
+  if (!oneTask) {
+    res.status(404).json({ error: "Task not found" });
+  } else {
+    res.status(200).json(oneTask);
+  }
+});
+
+//Create -
+tasks.post("/", async (req, res) => {
+  const body = req.body;
+  const newTask = await createTask(body);
+  res.status(200).json(newTask);
 });
 
 module.exports = tasks;
--- queries/tasks.js
+++ queries/tasks.js
@@ -9,8 +9,31 @@ const getAllTasks = async () => {
   }
 };
 
-// const getTask() {
+async function getTask(id) {
+  try {
+    const task = await db.one("SELECT * FROM tasks WHERE id=$1", id);
+    return task;
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-// }
-
-module.exports = { getAllTasks };
+async function createTask(task) {
+  try {
+    const newTask = await db.one(
+      "INSERT INTO tasks (name, description, deadline, category, assigned_to, status) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [
+        task.name,
+        task.description,
+        task.deadline,
+        task.category,
+        task.assigned_to,
+        task.status,
+      ]
+    );
+    return newTask;
+  } catch (e) {
+    console.log(e);
+  }
+}
+module.exports = { getAllTasks, getTask, createTask };
